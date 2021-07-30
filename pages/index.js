ebd06e6264357dc2bd8adc15ce0a366a7eb734f4
Alonso-Pablo <sidokunoice@gmail.com>
2021-07-30 13:41:35 -0300

    Testing optimizations

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,134 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import Head from 'next/head'
 import styled from 'styled-components';
-import { v4 as uuidv4 } from 'uuid';
+
+import { initialFrame30x30 } from '../initialFrame';
+
+const useCounter = () => {
+  const [ counter, setCounter ] = useState(0)
+  
+  const increase = () => setCounter(counter + 1);
+  const reset = () => setCounter(0);
+
+  return {
+    counter,
+    increase,
+    reset
+  };
+};
+
+const useBoolean = () => {
+  const [ boolean, setBoolean ] = useState(false);
+
+  const setInvert = () => setBoolean(!boolean);
+  const setTrue = () => setBoolean(true);
+  const setFalse = () => setBoolean(false);
+
+  return {
+    boolean,
+    setInvert,
+    setTrue,
+    setFalse,
+  };
+};
+
+// const useGame = (initialValue) => {
+//   const [ frame, setFrame ] = useState(initialValue);
+//   const savedFrame, setSavedFrame ] = useState(initialValue);
+
+//   const handleFrame = (indexRow, indexColumn) => {
+//     if (frame[indexRow][indexColumn] > 0) {
+//       return setFrame(frame[indexRow][indexColumn] = 0)
+//     }
+//     return
+//   };
+
+//   const memoizedFrame = useMemo(() =>
+//     frame.map((row, indexRow) => (
+//       row.map((cellular, indexColumn) => {
+//         const result = sumLife(frame, indexRow, indexColumn, cellular);
+//         if (result > 0) return setSavedFrame()
+//         ? savedFrame[indexRow][indexColumn] = 0
+//         : savedFrame[indexRow][indexColumn] = 1
+//       })
+//     ))
+//   , [frame])
+
+//   useEffect(() => {
+//     if (isGameRun.boolean) {
+//       generation.increase();
+
+//       setTimeout(() => {
+//         memoizedFrame()
+//       }, 200);
+//     }
+//   }, [isGameRun.boolean, frame])
+// }
+
 
 export default function Home() {
 
-  let [ isGameStarted, setIsGameStarted ] = useState(false);
+  const generation = useCounter();
+  const isGameRun = useBoolean();
+  const isConfigDisplay = useBoolean();
 
   const initialFrame = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ];
 
   const saveFrame = initialFrame;
 
-  let [ actualFrame, setActualFrame ] = useState(saveFrame);
+  const rowsTotal = saveFrame.length;
+  const columnsTotal = saveFrame[0].length;
+  const limitMap = {'x': (columnsTotal - 1), 'y': (rowsTotal - 1) };
 
-  let [ previusFrame, setPreviusFrame ] = useState(saveFrame);
+  let [ actualFrame, setActualFrame ] = useState(initialFrame);
 
-  let [ generationNumber, setGenerationNumber ] = useState(0);
+  // const ResizeArray = (index, decrease) => {
 
-  const limitMap = {'x': 9, 'y': 9};
+  //   if (decrease === 1) {
+  //     actualFrame.forEach((row) => row.splice(index, decrease));
+  //     actualFrame.splice(index, decrease);
+  //     console.log(actualFrame);
+  //     return actualFrame
+  
+  //   } else {
+  //     const element = 0;
+  //     actualFrame.forEach((row) => row.splice(index, decrease, element))
+  //     actualFrame.splice(index, decrease, actualFrame[0]);
+  //     console.log(actualFrame);
+  //     return actualFrame
+  //   }
+  // }
 
   const sumLife = (array, row, column, center) => {
     let NW = 0;
@@ -40,7 +140,7 @@ export default function Home() {
     let SW = 0;
     let W = 0;
 
-    /*
+  /*
       n[y-1][x-1]  n[y-1][x]  n[y-1][x+1]
 
        n[y][x-1]    n[y][x]    n[y][x+1]
@@ -147,8 +247,6 @@ export default function Home() {
     }
   }; // sumLife()
 
-
-
   const handleClick = (indexRow, indexColumn) => {
     actualFrame[indexRow][indexColumn] === 1
     ? setActualFrame(actualFrame[indexRow][indexColumn] = 0)
@@ -157,78 +255,95 @@ export default function Home() {
   };
 
   useEffect(() => {
-    if (isGameStarted) {
+    if (isGameRun.boolean) {
       setTimeout(() => {
-        setGenerationNumber(generationNumber += 1)
-        previusFrame.map((row, indexRow) => {
+        generation.increase();
+
+        actualFrame.map((row, indexRow) => {
           row.map((cellular, indexColumn) => {
-            const result = sumLife(previusFrame, indexRow, indexColumn, cellular);
-            result === 0
-            ? saveFrame[indexRow][indexColumn] = 0
-            : saveFrame[indexRow][indexColumn] = 1
+            return saveFrame[indexRow][indexColumn] = sumLife(actualFrame, indexRow, indexColumn, cellular);
           })
         })
-        setPreviusFrame(saveFrame);
         setActualFrame(saveFrame);
-        
-        return
-      }, 300)
+      }, 200)
     }
-  }, [isGameStarted, actualFrame])
+  }, [isGameRun.boolean, actualFrame])
 
   return (
     <>
       <Head>
         <title>Conway&apos;s Game of Life</title>
-        <meta name="description" content="Recreation of Conway&apos;s Game of Life" />
+        <meta name="description" content="Recreation of Conway&apos;s Game of Life in Next.js by Alonso Pablo" />
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
       </Head>
 
       <Main>
 
-        <Container>
-        {actualFrame.map((row, indexRow) => {
-            return row.map((cellular, indexColumn) =>
-              cellular === 0
-              ? <ButtonWhite onClick={() => handleClick(indexRow, indexColumn) } key={uuidv4()}></ButtonWhite>
-              : <ButtonBlack onClick={() => handleClick(indexRow, indexColumn) } key={uuidv4()}></ButtonBlack>
-            )
-          })
-        }
-        
-        </Container>
+        <ControllersContainer top="1px" justifyContent="flex-end">
+          { isConfigDisplay.boolean &&
+            <Configuration borderBottom="none" borderRadius="2px 2px 0 0">
 
-        <ControllersContainer>
+              <Button onClick={() => {isGameRun.setFalse(false); ResizeArray((rowsTotal - 1), 1)}}>-</Button>
 
-          <Controller fill="black" onClick={() => setIsGameStarted(!isGameStarted) }>
-            { !isGameStarted
+              <div>
+                <p>SIZE</p>
+                {`${rowsTotal} X ${columnsTotal}`}
+              </div>
+
+              <Button onClick={() => {isGameRun.setFalse(false); ResizeArray((rowsTotal - 1), 0)} }>+</Button>
+
+            </Configuration>
+          }
+          
+
+          <Controller onClick={() => isConfigDisplay.setInvert()} borderBottom="none" borderRadius="2px 2px 0 0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 15 15" fill="none">
+              <path d="M8.51562 1H6.89062L6.28125 2.42188L4.85938 3.03125L3.64062 2.42188L2.42188 3.64062L3.03125 5.0625L2.42188 6.28125L1 6.6875V8.3125L2.42188 8.92188L3.03125 10.1406L2.42188 11.5625L3.64062 12.7812L4.85938 12.1719L6.07812 12.5781L6.6875 14H8.51562L8.92188 12.5781L10.3438 12.1719L11.5625 12.7812L12.7812 11.5625L12.1719 10.1406L12.7812 8.92188L14 8.3125V6.6875L12.5781 6.28125L12.1719 4.85938L12.7812 3.64062L11.5625 2.42188L10.1406 3.03125L8.92188 2.42188L8.51562 1Z" stroke="black"/>
+              <circle cx="7.5" cy="7.5" r="2" stroke="black" />
+            </svg>
+          </Controller>
+
+        </ControllersContainer>
+
+        <Game rowsTotal={rowsTotal} columnsTotal={columnsTotal}>
+          {
+            actualFrame.map((row, indexRow) => {
+              return row.map((cellular, indexColumn) =>
+                cellular === 0
+                ? <Cellular cellular={cellular} onClick={() => handleClick(indexRow, indexColumn) } key={`row${indexRow}-column${indexColumn}`}></Cellular>
+                : <Cellular cellular={cellular} onClick={() => handleClick(indexRow, indexColumn) } key={`row${indexRow}-column${indexColumn}`}></Cellular>
+              )
+            })
+          }
+        </Game>
+
+        <ControllersContainer top="-1px" >
+
+          <Controller borderTop="none" borderRadius="0 0 2px 2px" fill="black" onClick={() => isGameRun.setInvert() }>
+            { !isGameRun.boolean
               ?
-                <>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 13 13" fill="none">
-                    <path d="M1 13V1.5L11 7.5L1 13Z" stroke="black"/>
-                  </svg>
-                  START
-                </>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 13 13" fill="none">
+                  <path d="M1 13V1.5L11 7.5L1 13Z" stroke="black"/>
+                </svg>
               : 
-                <>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 13 13" fill="none">
-                    <path d="M1 0.5V12H12.5V0.5H1Z" stroke="black"/>
-                  </svg>
-                  STOP
-                </>
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 13 13" fill="none">
+                  <path d="M1 0.5V12H12.5V0.5H1Z" stroke="black"/>
+                </svg>
             }
           </Controller>
 
-          <Controller>GENERATION:<br/>{generationNumber}</Controller>
+          <Controller borderTop="none" borderRadius="0 0 2px 2px">
+            GENERATION:<br/>{generation.counter}
+          </Controller>
 
-          <Controller onClick={() => {setActualFrame(initialFrame); setPreviusFrame(initialFrame); setGenerationNumber(0)} }>
+          <Controller borderTop="none" borderRadius="0 0 2px 2px" onClick={() => {setActualFrame(initialFrame); generation.reset()} }>
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 13 13" fill="none">
               <path d="M12 2.57139V5.19037L9.5 4.14278L10.9137 3.2542L12 2.57139Z" fill="black"/>
               <path d="M11 9.38074C10.5 10.4283 8.77701 12.0239 6.5 11.9997C3.32153 11.966 1 10.4283 1 6.76176C1 3.09519 3.5 1 6.5 1C9.5 1 10.5 2.57139 11.5 4.14278M12 5.19037V2.57139L9.5 4.14278L12 5.19037Z" stroke="black"/>
             </svg>
-            RESET
           </Controller>
+
         </ControllersContainer>
         
       </Main>
@@ -239,6 +354,20 @@ export default function Home() {
     </>
   )
 }
+
+
+const Cellular = styled.button`
+  height: 100%;
+  background-color: ${({cellular}) => cellular > 0 ? 'black' : 'white'};
+  border: 1px solid black;
+  cursor: pointer;
+`;
+
+// const ButtonWhite = styled(ButtonBlack)`
+//   background-color: white;
+//   border: 1px solid black;
+// `;
+
 const Main = styled.main`
   display: flex;
   width: 100%;
@@ -248,8 +377,7 @@ const Main = styled.main`
   align-items: center;
 `;
 
-
-const Container = styled.div`
+const Game = styled.div`
   box-sizing: border-box;
   display: grid;
   width: 90%;
@@ -257,8 +385,10 @@ const Container = styled.div`
   max-width: 300px;
   max-height: 300px;
   padding: 4px;
-  grid-template-rows: repeat(10, 1fr);
-  grid-template-columns: repeat(10, 1fr);
+  grid-template-rows: repeat(${({rowsTotal}) => rowsTotal || 10}, 1fr);
+  grid-template-columns: repeat(${({columnsTotal}) => columnsTotal || 10}, 1fr);
+  grid-auto-columns: 1fr;
+  grid-auto-rows: 1fr;
   gap: 2px;
   border: 1px solid black;
   border-radius: 2px;
@@ -275,25 +405,22 @@ const Container = styled.div`
   }
 `;
 
-const ButtonBlack = styled.button`
-  height: 100%;
-  background-color: black;
-  border: 1px solid black;
-  border-radius: 2px;
-  cursor: pointer;
-`;
-
-const ButtonWhite = styled(ButtonBlack)`
-  background-color: white;
-  border: 1px solid black;
-`;
-
 const ControllersContainer = styled.div`
+  width: 300px;
+  position: relative;
+  top: ${({top}) => top || ''};
   display: flex;
+  justify-content: ${({justifyContent}) => justifyContent || 'center'};
+
+  @media only screen and (min-width: 768px) {
+    width: 520px;
+  }
+  @media only screen and (min-width: 1910px) {
+    width: 1000px;
+  }
 `;
 
 const Controller = styled.button`
-  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -301,15 +428,15 @@ const Controller = styled.button`
   margin: 0 5px;
   background-color: white;
   border: 1px solid black;
-  border-top: none;
-  top: -1px;
+  border-top: ${({borderTop}) => borderTop || ''};
+  border-bottom: ${({borderBottom}) => borderBottom || ''};
+  border-radius: ${({borderRadius}) => borderRadius || ''};
   cursor: pointer;
   font-size: 1.1rem;
   font-weight: 700;
 
   & svg {
-    padding: 5px 0;
-    margin-right: 5px;
+    padding: 8px;
   }
 
   &:hover svg path {
@@ -317,12 +444,40 @@ const Controller = styled.button`
   }
 
   @media only screen and (min-width: 768px) {
-    height: 40px;
     margin: 0 15px;
 
   }
   @media only screen and (min-width: 1910px) {
-    height: 40px;
     margin: 0 20px;
+  }
+`;
+
+const Configuration = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 10px;
+  margin: 0 5px;
+  background-color: white;
+  border: 1px solid black;
+  border-bottom: ${({borderBottom}) => borderBottom || ''};
+  border-radius: ${({borderRadius}) => borderRadius || ''};
+
+  & div {
+    text-align: center;
+    padding: 0 5px
+  }
+`;
+
+const Button = styled.button`
+  padding: 4px 8px;
+  background-color: white;
+  border: 1px solid black;
+  border-radius: 2px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: black;
+    color: white;
   }
 `;
